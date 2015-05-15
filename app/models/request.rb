f56bcc5715a334eb4360660a60c94ccfc520f142
k4hun <thameth@gmail.com>
2015-05-15 18:05:16 +0200

    add callbacks for setting price and deadline

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,6 @@
 class Request < ActiveRecord::Base
   before_validation :set_as_unread
+  before_save :set_price, :set_deadline
 
   validates :description, :email, :category, presence: { message: 'Pole nie może być puste!' }
   CATEGORIES = ['awaria', 'uszkodzenie', 'usterka']
@@ -10,5 +11,35 @@ class Request < ActiveRecord::Base
   def set_as_unread
     self.read = false
     true
+  end
+
+  def set_price
+    case self.category
+    when 'awaria'
+      if Time.now >= 17 || Time.now <= 9
+        self.price = 100
+      else
+        self.price = 50
+      end
+    when 'uszkodzenie'
+      self.price = 10
+    when 'usterka'
+      self.price = 1
+    else
+      false
+    end
+  end
+
+  def set_deadline
+    case self.category
+    when 'awaria'
+      self.deadline = Time.now + 4.hours
+    when 'uszkodzenie'
+      self.deadline = Time.now + 1.day
+    when 'usterka'
+      self.deadline = Time.now + 3.days
+    else
+      false
+    end
   end
 end
