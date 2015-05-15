755f1331ee11c4b852d8b5248db687cdc3c8a165
k4hun <thameth@gmail.com>
2015-05-15 15:22:36 +0200

    create new request method

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,8 +1,19 @@
 class RequestsController < ApplicationController
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to root_path, notice: 'Zgłoszenie pomyślnie wysłane!'
+    else
+      render 'new', error: 'Nie udało się wysłać zgłoszenia!'
+    end
+  end
 
+  private
+  def request_params
+    params.require(:request).permit(:description, :category, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :requests, only: [:new]
+  resources :requests, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
