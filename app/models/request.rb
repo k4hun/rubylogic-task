8113da6886ff552d1740a38a393edd03532f6b97
k4hun <thameth@gmail.com>
2015-05-15 15:23:27 +0200

    fix validations and add error messages

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,12 +1,14 @@
 class Request < ActiveRecord::Base
-  before_save :set_as_unread
+  before_validation :set_as_unread
 
-  validates :description, :email, :category, presence: true
-  validates :category, inclusion: { in: %w(awaria, uszkodzenie, usterka) }
-  validates :description, length: { minimum: 100 }
-  validates :email, format: { with: (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/) }
+  validates :description, :email, :category, presence: { message: 'Pole nie może być puste!' }
+  CATEGORIES = ['awaria', 'uszkodzenie', 'usterka']
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :description, length: { minimum: 100, message: 'Pole musi zawierać minimum 100 znaków!' }
+  validates :email, format: { with: (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/), message: 'Zły format adresu email!' }
 
   def set_as_unread
     self.read = false
+    true
   end
 end
