5d798993543c5a6123a37bdb6fe033ddedfa7cb6
k4hun <thameth@gmail.com>
2015-05-15 13:02:29 +0200

    create request model

---
+++ db/migrate/20150515103457_create_requests.rb
@@ -0,0 +1,13 @@
+class CreateRequests < ActiveRecord::Migration
+  def change
+    create_table :requests do |t|
+      t.string :description
+      t.string :category
+      t.string :email
+      t.decimal :price
+      t.datetime :deadline
+      t.boolean :read
+      t.timestamps null: false
+    end
+  end
+end
