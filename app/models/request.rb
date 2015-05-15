592f585c3acfb66a59296d8d3130f0c168ddc601
k4hun <thameth@gmail.com>
2015-05-15 13:29:29 +0200

    add validations to request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,12 @@
 class Request < ActiveRecord::Base
+  before_save :set_as_unread
+
+  validates :description, :email, :category, presence: true
+  validates :category, inclusion: { in: %w(awaria, uszkodzenie, usterka) }
+  validates :description, length: { maximum: 100 }
+  validates :email, format: { with: (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/) }
+
+  def set_as_unread
+    self.read = false
+  end
 end
