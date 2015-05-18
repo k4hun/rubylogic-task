0b00fd4e99b44e0ac9cd3574b9140399f69c8029
k4hun <thameth@gmail.com>
2015-05-18 12:54:21 +0200

    merged two callbacks into one method

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ActiveRecord::Base
-  before_save :set_price, :set_deadline
+  before_save :set_price_and_deadline
 
   validates :description, :email, :category, presence: { message: 'Pole nie może być puste!' }
   CATEGORIES = ['awaria', 'uszkodzenie', 'usterka']
@@ -7,7 +7,7 @@ class Request < ActiveRecord::Base
   validates :description, length: { minimum: 100, message: 'Pole musi zawierać minimum 100 znaków!' }
   validates :email, format: { with: (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/), message: 'Zły format adresu email!' }
 
-  def set_price
+  def set_price_and_deadline
     case self.category
     when 'awaria'
       if Time.now.hour >= 17 || Time.now.hour <= 9
@@ -15,22 +15,12 @@ class Request < ActiveRecord::Base
       else
         self.price = 50
       end
-    when 'uszkodzenie'
-      self.price = 10
-    when 'usterka'
-      self.price = 1
-    else
-      false
-    end
-  end
-
-  def set_deadline
-    case self.category
-    when 'awaria'
       self.deadline = Time.now + 4.hours
     when 'uszkodzenie'
+      self.price = 10
       self.deadline = Time.now + 1.day
     when 'usterka'
+      self.price = 1
       self.deadline = Time.now + 3.days
     else
       false
